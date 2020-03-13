895bbdb947c2f03734cd82e6c47d368d4d96c2c8
george-g <georgegombert@gmail.com>
2020-03-13 00:09:14 -0600

    Updated js with all buttons and form areas now functioning on page. Passwords can be generated

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,127 @@
-var sliderValue = document.getElementById('paswordCharacterCount');
-var passwordCharacterCount  = parseInt(sliderValue.value);
+// Script for gassword generation. The script defines all the variables, sets up the interactive elements 
+// of the page, then goes through the main password generation function.
 
-var includeUpperCase = document.getElementById('includeUpperCase').checked;
-var includeNumbers = document.getElementById('includeNumbers').checked;
-var includeSpecial = document.getElementById('includeSpecial').checked;
+var sliderValue = document.getElementById('paswordCharacterCount'); //range slider in form
+var passwordCharacterCount  = parseInt(sliderValue.value); // turning sliver string return into an integer
 
+//variable declarations for the check boxes
+var includeUpperCase = document.getElementById('includeUpperCase'); // have to figure out how to set initial value or read initial value
+var includeNumbers = document.getElementById('includeNumbers');
+var includeSpecial = document.getElementById('includeSpecial');
+
+// creating the arrays for all the possible password characters
 var baseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var baseUpperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 var baseNumbers = ['0','1','2','3','4','5','6','7','8','9'];
 var baseSpecial = ["U+0021","U+0022"];
 
+// event listeners for when the form is manipulated (slider & check boxes)
+includeUpperCase.addEventListener( 'change', function() {
+    if(this.checked) {
+        includeUpperCase = true;
+    } else {
+        includeUpperCase = false;
+    }
+})
+
+includeNumbers.addEventListener( 'change', function() {
+    if(this.checked) {
+        includeNumbers = true;
+    } else {
+        includeNumbers = false;
+    }
+})
+
+includeSpecial.addEventListener( 'change', function() {
+    if(this.checked) {
+        includeSpecial = true;
+    } else {
+        includeSpecial = false;
+    }
+})
+
+sliderValue.addEventListener('change', function(){
+    sliderValue = document.getElementById('paswordCharacterCount').value;
+    passwordCharacterCount  = parseInt(sliderValue);
+    console.log(sliderValue , passwordCharacterCount);
+})
+
+
+
 
 function generatePassword(){
-    var password = [];
+    var password = []; //empty array that is filled by the different if statements
+    
+    //if statement secton for all possible checkbox variations
+    if(includeUpperCase == true && includeNumbers == false && includeSpecial == false){
+        var passwordArray = baseCharacters.concat(baseUpperCase);
+        for(var i=0; i<passwordCharacterCount; i++){
+            password[i] = passwordArray[Math.floor(Math.random() * passwordArray.length)];
+        } 
+        password = password.join('');
+        document.getElementById('passwordBox').textContent = password;
+    } 
+    
+    else if(includeUpperCase == false && includeNumbers == true && includeSpecial == false){
+        var passwordArray = baseCharacters.concat(baseNumbers);
+        for(var i=0; i<passwordCharacterCount; i++){
+            password[i] = passwordArray[Math.floor(Math.random() * passwordArray.length)];
+        } 
+        password = password.join('');
+        document.getElementById('passwordBox').textContent = password;
+    }
 
-    if(includeUpperCase == false && includeNumbers == false && includeSpecial == false){
+    else if(includeUpperCase == false && includeNumbers == false && includeSpecial == true){
+        var passwordArray = baseCharacters.concat(baseSpecial);
+        for(var i=0; i<passwordCharacterCount; i++){
+            password[i] = passwordArray[Math.floor(Math.random() * passwordArray.length)];
+        } 
+        password = password.join('');
+        document.getElementById('passwordBox').textContent = password;
+    }
+
+    else if(includeUpperCase == true && includeNumbers == true && includeSpecial == false){
+        var passwordArray = baseCharacters.concat(baseUpperCase,baseNumbers);
+        for(var i=0; i<passwordCharacterCount; i++){
+            password[i] = passwordArray[Math.floor(Math.random() * passwordArray.length)];
+        } 
+        password = password.join('');
+        document.getElementById('passwordBox').textContent = password;
+    }
+
+    else if(includeUpperCase == true && includeNumbers == false && includeSpecial == true){
+        var passwordArray = baseCharacters.concat(baseUpperCase,baseSpecial);
+        for(var i=0; i<passwordCharacterCount; i++){
+            password[i] = passwordArray[Math.floor(Math.random() * passwordArray.length)];
+        } 
+        password = password.join('');
+        document.getElementById('passwordBox').textContent = password;
+    }
+
+    else if(includeUpperCase == false && includeNumbers == true && includeSpecial == true){
+        var passwordArray = baseCharacters.concat(baseNumbers,baseSpecial);
+        for(var i=0; i<passwordCharacterCount; i++){
+            password[i] = passwordArray[Math.floor(Math.random() * passwordArray.length)];
+        } 
+        password = password.join('');
+        document.getElementById('passwordBox').textContent = password;
+    }
+
+    else if(includeUpperCase == true && includeNumbers == true && includeSpecial == true){
+        var passwordArray = baseCharacters.concat(baseUpperCase,baseNumbers,baseSpecial);
+        for(var i=0; i<passwordCharacterCount; i++){
+            password[i] = passwordArray[Math.floor(Math.random() * passwordArray.length)];
+        } 
+        password = password.join('');
+        document.getElementById('passwordBox').textContent = password;
+    }
+
+    else{
         for(var i=0; i<passwordCharacterCount; i++){
             password[i] = baseCharacters[Math.floor(Math.random() * baseCharacters.length)];
         } 
         password = password.join('');
-        console.log(password);
+        document.getElementById('passwordBox').textContent = password;
     }
     
-}
+}//end of generatePassword function 
